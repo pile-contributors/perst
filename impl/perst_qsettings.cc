18c3a795029d2f1deba938abadc02d63c606d4d1
Nicu Tofan <nicu.tofan@gmail.com>
2015-10-17 17:49:12 +0300

    cosmetic

--- impl/perst_qsettings.cc
+++ impl/perst_qsettings.cc
@@ -55,8 +55,10 @@ PerStQSettings::~PerStQSettings()
 {
     PERST_TRACE_ENTRY;
 
+    backend_.sync ();
 #ifdef PERST_DEBUG
-    printf ("Final settings content for PerSt file %s\n", PERST_STRING_TO_TMPC(location()));
+    printf ("Final settings content for PerSt file %s\n",
+            PERST_STRING_TO_TMPC(location()));
     foreach(const QString & skey, backend_.allKeys()) {
         printf ("   - %s = %s\n",
                 PERST_STRING_TO_TMPC(skey),
