189f5c80814bd5f761de13e847401969cfebf194
Nicu Tofan <nicu.tofan@gmail.com>
2016-01-04 13:56:37 +0200

    Improve

--- perst-private.h
+++ perst-private.h
@@ -1,7 +1,7 @@
 /**
  * @file perst-private.h
  * @brief Declarations for PerSt class
- * @author 
+ * @author Nicu Tofan <nicu.tofan@gmail.com>
  * @copyright Copyright 2014 piles contributors. All rights reserved.
  * This file is released under the
  * [MIT License](http://opensource.org/licenses/mit-license.html)
